58a11d4e70cd4b8f624ec875c61456120f2b12f0
usman2195@gmail.com <md.usman2195@gmail.com>
2022-12-27 13:28:10 +0530

    umair bhai change

--- src/main/java/Page/CollectionPage.java
+++ src/main/java/Page/CollectionPage.java
@@ -15,7 +15,7 @@ import Utility.TestUtils;
 public class CollectionPage 
 {
 	WebDriver driver;
-//	done
+//	done umair bhai
 	
 	//first product
 	@FindBy(xpath = "//a[@href='/products/101-t-shirt']")
